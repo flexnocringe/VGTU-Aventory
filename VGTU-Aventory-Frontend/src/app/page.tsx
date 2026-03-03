e383d97e9aa52238bce28af6eaef7f6a6508d09d
Borya Vilka <damian.karalkevic@gmail.com>
2026-03-03 23:14:42 +0200

    Empty backend project commit

---
+++ VGTU-Aventory-Frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <h1 className="text-4xl font-bold text-center text-black">Hello World</h1>
+      </div>
+    </div>
+  );
+}
