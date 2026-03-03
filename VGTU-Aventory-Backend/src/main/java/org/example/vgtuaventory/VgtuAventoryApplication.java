5aea5326acc1ea92a7987daf21846d045352f1a7
Borya Vilka <damian.karalkevic@gmail.com>
2026-03-03 21:16:25 +0200

    Empty backend project commit

---
+++ VGTU-Aventory-Backend/src/main/java/org/example/vgtuaventory/VgtuAventoryApplication.java
@@ -0,0 +1,13 @@
+package org.example.vgtuaventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VgtuAventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VgtuAventoryApplication.class, args);
+    }
+
+}
---
+++ VGTU-Aventory-Backend/src/test/java/org/example/vgtuaventory/VgtuAventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vgtuaventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VgtuAventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
