47f0fccac56fbcae25b7ffaa8ef8e081f0321838
MYSERY-MACHINE\HP <damian.karalkevic@gmail.com>
2026-03-11 20:14:56 +0200

    FEATURE_DB-6: Pajamu saraso nuskaitymas is duomenu bazes

---
+++ VGTU-Aventory-Backend/src/main/java/org/example/vgtuaventory/controllers/TotalSalesController.java
@@ -0,0 +1,46 @@
+package org.example.vgtuaventory.controllers;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import org.example.vgtuaventory.model.Sale;
+import org.example.vgtuaventory.repositories.SaleRepository;
+import org.example.vgtuaventory.utils.LocalDateAdapter;
+import org.example.vgtuaventory.utils.LocalDateTimeAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class TotalSalesController {
+    @Autowired
+    private SaleRepository saleRepository;
+
+    @GetMapping(value  = "/allSalesInPeriod")
+    public @ResponseBody Map<Integer, Double> findAllBySaleDateBetween(@RequestBody String dateInfo) {
+        GsonBuilder build = new GsonBuilder();
+        build.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
+        Gson gson = build.setPrettyPrinting().create();
+        JsonObject dateInterval = gson.fromJson(dateInfo, JsonObject.class);
+        LocalDateTime startDate = LocalDate.parse(dateInterval.get("startDate").getAsString()).atStartOfDay();
+        LocalDateTime endDate = LocalDate.parse(dateInterval.get("endDate").getAsString()).atStartOfDay();
+        List<Sale> sales = saleRepository.findAllBySaleDateBetween(startDate, endDate);
+        Map<Integer, Double> totalProfit = new HashMap<>();
+        for(Sale sale : sales) {
+            totalProfit.put(sale.getId(), sale.getTotalPrice());
+        }
+        return totalProfit;
+    }
+
+}
---
+++ VGTU-Aventory-Backend/src/main/java/org/example/vgtuaventory/utils/LocalDateAdapter.java
@@ -0,0 +1,20 @@
+package org.example.vgtuaventory.utils;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+
+public class LocalDateAdapter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
+
+    @Override
+    public JsonElement serialize(LocalDate date, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(date.toString()); // ISO format: 2025-12-09
+    }
+
+    @Override
+    public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+            throws JsonParseException {
+        return LocalDate.parse(json.getAsString());
+    }
+}
