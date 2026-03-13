5064dc3c789f8114d71a6e1a186d3ef17ac6ce16
Devidas <deividasnarbutas2@gmail.com>
2026-03-13 16:32:17 +0200

    FEATURE_DB-5 Naudotojo suradimas DB pagal pasta

---
+++ VGTU-Aventory-Backend/src/main/java/org/example/vgtuaventory/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.example.vgtuaventory.repository;
+
+import org.example.vgtuaventory.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findbyEmail(String email);
+}
