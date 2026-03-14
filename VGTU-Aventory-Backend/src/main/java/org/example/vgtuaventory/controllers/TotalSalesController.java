bdd7298273bc9fc354d89063162f070416425029
MYSERY-MACHINE\HP <damian.karalkevic@gmail.com>
2026-03-14 15:22:00 +0200

    FEAT_Backend-49: Sukurti pajamu sumavimo laiko tarpe funkcija

--- VGTU-Aventory-Backend/src/main/java/org/example/vgtuaventory/controllers/TotalSalesController.java
+++ VGTU-Aventory-Backend/src/main/java/org/example/vgtuaventory/controllers/TotalSalesController.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import javafx.util.Pair;
 import org.example.vgtuaventory.model.Sale;
 import org.example.vgtuaventory.repositories.SaleRepository;
 import org.example.vgtuaventory.utils.LocalDateAdapter;
@@ -41,6 +42,24 @@ public class TotalSalesController {
             totalProfit.put(sale.getId(), sale.getTotalPrice());
         }
         return totalProfit;
+    }
+
+    @GetMapping(value = "/totalProfitInInterval")
+    public @ResponseBody String getTotalProfitInInterval(@RequestBody String dateInfo) {
+        GsonBuilder build = new GsonBuilder();
+        build.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
+        Gson gson = build.setPrettyPrinting().create();
+        JsonObject dateInterval = gson.fromJson(dateInfo, JsonObject.class);
+        LocalDateTime startDate = LocalDate.parse(dateInterval.get("startDate").getAsString()).atStartOfDay();
+        LocalDateTime endDate = LocalDate.parse(dateInterval.get("endDate").getAsString()).atStartOfDay();
+        List<Sale> sales = saleRepository.findAllBySaleDateBetween(startDate, endDate);
+        Double sum = 0.0;
+        for(Sale sale : sales) {
+            sum += sale.getTotalPrice();
+        }
+        JsonObject totalProfit = new JsonObject();
+        totalProfit.addProperty("totalProfit", sum);
+        return gson.toJson(totalProfit);
     }
 
 }
