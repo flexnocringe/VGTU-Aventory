9ab7a92eeeb5af1326642e704a457a2a1e0ddf5e
Ana <anastasija.bedenko@stud.vilniustech.lt>
2026-04-27 19:43:38 +0300

    Backend_108: Renamed product_category to categoryName

--- VGTU-Aventory-Backend/src/main/java/org/example/vgtuaventory/model/Product.java
+++ VGTU-Aventory-Backend/src/main/java/org/example/vgtuaventory/model/Product.java
@@ -30,5 +30,5 @@ public class Product {
     protected String photoUrl;
     protected int quantity;
     protected String qrCode;
-    protected int product_category;
+    protected int categoryName;
 }
