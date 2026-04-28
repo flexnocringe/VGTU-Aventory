d340e95dfb8f42e2d8404cceddce47fa2d5d89bd
Devidas <deividasnarbutas2@gmail.com>
2026-04-28 09:50:31 +0300

    fix: products now accept http image links

--- VGTU-Aventory-Frontend/next.config.ts
+++ VGTU-Aventory-Frontend/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**" },
+    ],
+  },
   async rewrites() {
     return [
       {
